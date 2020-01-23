cc5c48a3736d868f049ccd4656441d9ef67fe377
SHIMIN TAN <releski@hotmail.com>
2020-01-23 11:44:01 +0800

    Initial Commit

---
+++ MyTools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTools
+//
+//  Created by Shimin Tan on 23/1/20.
+//  Copyright © 2020 Shimin Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
